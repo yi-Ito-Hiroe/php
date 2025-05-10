5e6d4e3196f5483acce707e6799d37fc177527e8
Itou Hiroe <ito_hiroe@y-i-group.co.jp>
2025-05-11 00:09:14 +0900

    add:1から100を表示させるphpを作成

---
+++ q3/answer.php
@@ -0,0 +1,16 @@
+<!-- PHP課題③：1〜100までの数字を繰り返し出力しよう -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>q3</title>
+</head>
+<body>
+<?php
+for($i = 1; $i <= 100; $i++){
+ echo $i . "<br>";
+}
+?>
+</body>
+</html>
